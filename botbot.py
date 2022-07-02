6b8e0f2c8c54a619f9e28c8d25adee9d7c2a78ef
Char * <63970461+Chardelyce@users.noreply.github.com>
2022-07-02 07:52:52 -0500

    Create botbot.py

---
+++ botbot.py
@@ -0,0 +1,6 @@
+print('Quasar')
+print('the most basic programming language')
+print('if you have a very limited keyboard')
+print('chardelyce edwards 2022')
+print('https://github.com/chardelyce/Quasar')
+print('--------------------------------')
