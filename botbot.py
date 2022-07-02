64c8b54829bbd8d0d9b82fd64c1e503855c3f979
Char * <63970461+Chardelyce@users.noreply.github.com>
2022-07-02 12:58:48 -0500

    added lex

--- botbot.py
+++ botbot.py
@@ -1,3 +1,6 @@
+
+from sly import Lexer 
+
 """ 
  .::::::.    ...    :::  :::.     .::::::.   :::.    :::::::..   
 ,;;'```';;,  ;;     ;;;  ;;`;;   ;;;`    `   ;;`;;   ;;;;``;;;;  
@@ -12,6 +15,36 @@
                 created by: Chardleyce Edwards  
  """
 
+class Protons(Lexer):
+    tokens = { NAME, NUMBER, STRING }
+    ignore = '\t '
+    literals = { '=', '+', '-', '/', 
+                '*', '(', ')', ',', ';'}
+  
+  
+    
+    NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
+    STRING = r'\".*?\"'
+  
+    
+    @_(r'\d+')
+    def NUMBER(self, t):
+        
+        
+        t.value = int(t.value) 
+        return t
+  
+    
+    @_(r'//.*')
+    def COMMENT(self, t):
+        pass
+  
+  
+    @_(r'\n+')
+    def newline(self, t):
+        self.lineno = t.value.count('\n')
+
+
 
 print('      • Quasar                       ')
 print('                           ')
@@ -21,9 +54,4 @@ print('                           ')
 print('chardelyce edwards 2022')
 print('https://github.com/chardelyce/Quasar')
 print('--------------------------------')
-
-
-
-
-
 
