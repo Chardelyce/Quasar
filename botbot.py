3075ffa6783653ffd0f34d9c33a414905f1d27c5
Char * <63970461+Chardelyce@users.noreply.github.com>
2022-07-02 08:21:27 -0500

    changes

--- botbot.py
+++ botbot.py
@@ -1,6 +1,29 @@
-print('Quasar')
-print('the most basic programming language')
-print('if you have a very limited keyboard')
+""" 
+ .::::::.    ...    :::  :::.     .::::::.   :::.    :::::::..   
+,;;'```';;,  ;;     ;;;  ;;`;;   ;;;`    `   ;;`;;   ;;;;``;;;;  
+[[[     [[[\[['     [[[ ,[[ '[[, '[==/[[[[, ,[[ '[[,  [[[,/[[['  
+"$$c  cc$$$"$$      $$$c$$$cc$$$c  '''    $c$$$cc$$$c $$$$$$c    
+ "*8bo,Y88b,88    .d888 888   888,88b    dP 888   888,888b "88bo,
+   "*YP" "M" "YmmMMMM"" YMM   ""`  "YMmMY"  YMM   ""` MMMM   "W" 
+            
+            "the most basic programming language 
+            if you have a very limited keyboard"
+                            
+                created by: Chardleyce Edwards  
+ """
+
+
+print('      • Quasar                       ')
+print('                           ')
+print('"the most basic programming language')
+print('if you have a very limited keyboard"')
+print('                           ')
 print('chardelyce edwards 2022')
 print('https://github.com/chardelyce/Quasar')
 print('--------------------------------')
+
+
+
+
+
+
